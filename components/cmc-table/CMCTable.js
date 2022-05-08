ec0a7e0be23de766f8b05f16da7465213a7ae21f
baby123 <>
2022-05-08 23:35:03 +0800

    update

--- components/cmc-table/CMCTable.js
+++ components/cmc-table/CMCTable.js
@@ -1,7 +1,8 @@
 import React,{useState, useContext,useEffect, useCallback} from 'react'
 import btc from '../../assets/btc.png'
 import { CoinMarketContext} from '../../context/context'
-
+import CMCTableHeader from './CMCTableHeader'
+import CMCTableRow from './CMCTableRow'
 
 function CMCTable() {
     let {getTopTenCoins} = useContext(CoinMarketContext)
@@ -28,7 +29,38 @@ function CMCTable() {
     },[getTopTenCoins])
     console.log(coinData)
   return (
-    <div>CMCTable</div>
+    <div className='text-white font-bold'>
+      <div className='mx-auto max-w-screen-2xl'>
+        <table className='w-full'>
+          <CMCTableHeader />
+
+          {coinData && coinData ? (
+            coinData.map((coin, index) => {
+              return (
+                <CMCTableRow
+                  key={index}
+                  starNum={coin.cmc_rank}
+                  coinName={coin.name}
+                  coinSymbol={coin.symbol}
+                  coinIcon={btc}
+                  showBuy={true}
+                  hRate={coin.quote.USD.percent_change_24h}
+                  dRate={coin.quote.USD.percent_change_7d}
+                  hRateIsIncrement={true}
+                  price={coin.quote.USD.price}
+                  marketCapValue={coin.quote.USD.market_cap}
+                  volumeCryptoValue={coin.quote.USD.volume_24h}
+                  volumeValue={coin.total_supply}
+                  circulatingSupply={coin.circulating_supply}
+                />
+              )
+            })
+          ) : (
+            <></>
+          )}
+        </table>
+      </div>
+    </div>
   )
 }
 
--- pages/api/getTopTen.js
+++ pages/api/getTopTen.js
@@ -3,20 +3,23 @@ export default function handler(req, res){
     const getData = async () => {
         console.log('start to get Data')
         //${process.env.CMC_API_KEY}
+        
         const response = await fetch(
-            `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=436059cd-d8c8-4e80-80a8-ee606c28fa4e`,
+            'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=436059cd-d8c8-4e80-80a8-ee606c28fa4e',
             {
-            method: 'GET',
-            headers: {
-                Accept: '*/*',
-            },
+                method: 'GET',
+                headers: {
+                    Accept: '*/*',
+                },
             },
         )
-
-    
+        //let json = require('../../data/vendor.json')
+        //const data = json//await response.json()
         const data = await response.json()
         console.log('cmc api data',data)
         res.status(200).json({ data })
+
+        
     }
 
     getData()
